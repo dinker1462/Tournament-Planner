76fa19a980ae302475eb611b39a260c4e4c85480
Dinker <Dinker@maverick.local>
2017-04-02 13:15:33 +0530

    second commit

--- tournament.py
+++ tournament.py
@@ -15,7 +15,7 @@ def deleteMatches():
     """Remove all the match records from the database."""
     conn = connect()
     c = conn.cursor()
-    c.execute("UPDATE Matches SET wins = 0, matchesplayed = 0;")
+    c.execute("DELETE FROM matches;")
     conn.commit()
     conn.close()
 
@@ -25,7 +25,7 @@ def deletePlayers():
 
     conn = connect()
     c = conn.cursor()
-    c.execute("DELETE FROM Players;")
+    c.execute("DELETE FROM players;")
     conn.commit()
     conn.close()
 
@@ -36,7 +36,7 @@ def countPlayers():
     conn = connect()
     c = conn.cursor()
     player_count = 0
-    c.execute("select count(*) as pcount from Players;")
+    c.execute("select count(*) as pcount from players;")
     player_count = int(c.fetchone()[0])
     conn.commit()
     conn.close()
@@ -49,11 +49,7 @@ def registerPlayer(name):
     conn = connect()
     c = conn.cursor()
     d = conn.cursor()
-    c.execute("INSERT INTO Players(name) values(%s);", (name,))
-    c.execute("SELECT max(pid) from Players")
-    pid = int(c.fetchone()[0])
-    d.execute("""INSERT INTO Matches(pid, matchesplayed, wins)
-              values(%s, 0, 0);""", (pid,))
+    c.execute("INSERT INTO players(name) values(%s);", (name,))
     conn.commit()
     conn.close()
 
@@ -63,9 +59,7 @@ def playerStandings():
 
     conn = connect()
     c = conn.cursor()
-    c.execute("""SELECT Players.pid, Players.name, Matches.wins,
-              Matches.matchesplayed FROM Players LEFT JOIN Matches on
-              Players.pid=Matches.pid ORDER BY Matches.wins;""")
+    c.execute("SELECT * FROM standings;")
     player_standings = c.fetchall()
     conn.commit()
     conn.close()
@@ -77,11 +71,7 @@ def reportMatch(winner, loser):
 
     conn = connect()
     w = conn.cursor()
-    l = conn.cursor()
-    w.execute("""UPDATE Matches SET wins=wins + 1,
-              matchesplayed=matchesplayed + 1 WHERE pid=%s""", (winner,))
-    l.execute("""UPDATE Matches SET matchesplayed = matchesplayed + 1 WHERE
-              pid = %s """, (loser,))
+    w.execute("INSERT INTO matches(winner_id, loser_id) values(%s, %s);", (winner, loser))
     conn.commit()
     conn.close()
 
@@ -91,8 +81,7 @@ def swissPairings():
 
     conn = connect()
     c = conn.cursor()
-    c.execute("""SELECT Players.pid, Players.name FROM Players LEFT JOIN Matches on
-              Players.pid=Matches.pid ORDER BY Matches.wins""")
+    c.execute("SELECT standings.id, standings.name FROM standings")
     players = c.fetchall()
     conn.commit()
     conn.close()
--- tournament.sql
+++ tournament.sql
@@ -5,16 +5,37 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+DROP DATABASE tournament;
+
 CREATE DATABASE tournament;
+
 \c tournament;
 
-CREATE TABLE Players(
-    PID serial PRIMARY KEY,
-    NAME TEXT
+CREATE TABLE Players (
+    id serial PRIMARY KEY,
+    name TEXT
 );
 
-CREATE TABLE Matches(
-    PID serial PRIMARY KEY,
-    WINS INT,
-    MATCHESPLAYED INT
+CREATE TABLE matches (
+   match_id SERIAL PRIMARY KEY,
+   winner_id INT REFERENCES players(id) ON DELETE CASCADE,
+   loser_id INT REFERENCES players(id) ON DELETE CASCADE,
+   CHECK (winner_id <> loser_id)
 );
+
+CREATE VIEW winner_count AS
+SELECT players.id, players.name, COUNT(matches.winner_id) AS total_wins
+FROM players LEFT JOIN matches
+ON players.id = matches.winner_id
+GROUP BY players.id;
+
+CREATE VIEW matches_count AS
+SELECT players.id, players.name, COUNT(matches) AS total_matches
+FROM players LEFT JOIN matches
+ON players.id = matches.winner_id OR players.id = matches.loser_id
+GROUP BY players.id;
+
+CREATE OR REPLACE VIEW standings AS
+SELECT winner_count.id, winner_count.name, winner_count.total_wins, matches_count.total_matches
+FROM winner_count JOIN matches_count ON winner_count.id = matches_count.id
+ORDER BY winner_count.total_wins DESC;
